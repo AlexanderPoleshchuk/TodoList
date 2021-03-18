6730819886bfc0121977da6cb573ab4dc88151c9
Alexander <poleshchukalexander@gmail.com>
2021-03-18 19:32:34 +0300

    add task model

--- taskapp/models.py
+++ taskapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Заголовок')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    is_completed = models.BooleanField(default=False, verbose_name='Завершено')
+    # author = models.ForeignKey('User', on_delete=models.PROTECT, verbose_name='Пользователь')
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+        ordering = ['-created_at']
+
+
