3e8eb49eb66f475e24d176177a9f481c2315ea18
Alexander <poleshchukalexander@gmail.com>
2021-03-21 22:09:25 +0300

    fix forms.py

--- taskapp/forms.py
+++ taskapp/forms.py
@@ -1,13 +1,12 @@
 from django import forms
 from .models import Task
 
-class TaskForm(forms.ModelForm):
 
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ('title', 'description', 'photo')
         widgets = {
-            'title': forms.TextInput(attrs={'class': "form-control", 'placeholder': "Заголовок" }),
+            'title': forms.TextInput(attrs={'class': "form-control", 'placeholder': "Заголовок"}),
             'description': forms.Textarea(attrs={'class': "form-control", 'placeholder': "Описание"}),
         }
-
