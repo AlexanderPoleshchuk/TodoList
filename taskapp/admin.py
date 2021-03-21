1604ee7030aeeff7686a927504523cb7b486cc5e
Alexander <poleshchukalexander@gmail.com>
2021-03-21 22:08:32 +0300

    fix admin.py

--- taskapp/admin.py
+++ taskapp/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Task
 
-# Register your models here.
-
 
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'created_at', 'is_completed')
@@ -10,7 +8,6 @@ class TaskAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description')
     list_editable = ('is_completed',)
     list_filter = ('is_completed',)
-
 
 
 admin.site.register(Task, TaskAdmin)
