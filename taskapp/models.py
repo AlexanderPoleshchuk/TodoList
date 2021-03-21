0812544c7f1212f8469759949613cd1f33158d6e
Alexander <poleshchukalexander@gmail.com>
2021-03-21 22:10:00 +0300

    fix models.py

--- taskapp/models.py
+++ taskapp/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.urls import reverse_lazy
 
-# Create your models here.
-
 
 class Task(models.Model):
     title = models.CharField(max_length=150, verbose_name='Заголовок')
@@ -10,11 +8,11 @@ class Task(models.Model):
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     is_completed = models.BooleanField(default=False, verbose_name='Выполнено', blank=True)
+
     # author = models.ForeignKey('User', on_delete=models.PROTECT, verbose_name='Пользователь')
 
     def get_absolute_url(self):
         return reverse_lazy('task', kwargs={'pk': self.pk})
-
 
     def __str__(self):
         return self.title
@@ -23,5 +21,3 @@ class Task(models.Model):
         verbose_name = 'Задача(y)'
         verbose_name_plural = 'Задачи'
         ordering = ['-created_at']
-
-
