bc0278a6530584265e196b791a7d32db18e75506
Alexander <poleshchukalexander@gmail.com>
2021-03-26 00:31:54 +0300

    fix settings

--- TodoList/settings.py
+++ TodoList/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '90m1)0whbgofbcsu_6(t&b=7y-0=an7&spt8=uk*=)tx8)wt(8'
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
